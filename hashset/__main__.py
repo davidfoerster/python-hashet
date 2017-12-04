47de88a63277a31f6b55eb9db263debb983cd9e9
David Foerster <david.foerster@informatik.hu-berlin.de>
2017-12-04 05:05:21 +0100

    Support writing hashset to stdout

--- hashset/__main__.py
+++ hashset/__main__.py
@@ -7,24 +7,42 @@ pickle.DEFAULT_PROTOCOL = pickle.HIGHEST_PROTOCOL
 
 
 def _open(path, mode='r'):
-	if path == '-':
-		if mode == 'r':
-			return sys.stdin
-		if mode == 'w':
-			return sys.stdout
+	if path != '-':
+		return open(path, mode)
 
-		ValueError(
-			'Cannot use the special path {!r} with file mode {!r}. '
-			'Please use \'/dev/std*\' etc. instead.'
-				.format(path, mode))
+	mode = set(mode)
+	mode.discard('t')
 
-	return open(path, mode)
+	if 'r' in mode:
+		f_src = (sys, 'stdin')
+	elif 'w' in mode or 'a' in mode:
+		f_src = (sys, 'stdout')
+	else:
+		f_src = None
+
+	if f_src is not None:
+		f = getattr(*f_src)
+		if 'b' in mode and 'b' not in f.mode:
+			wrapper = f
+			f = f.buffer
+		else:
+			wrapper = None
+		if len(mode) == len(f.mode) and mode.issubset(f.mode):
+			if wrapper is not None:
+				f = wrapper.detach()
+			setattr(*(f_src + (None,)))
+			return f
+
+	ValueError(
+		'Cannot use the special path {!r} with file mode {!r}. '
+		'Please use \'/dev/std*\' etc. instead.'
+			.format(path, mode))
 
 
 args = sys.argv[1:]
 if len(args) == 3 and args[0] == '--build':
 	from .picklers import string_pickler
-	with _open(args[1]) as f_in, open(args[2], 'wb') as f_out:
+	with _open(args[1]) as f_in, _open(args[2], 'wb') as f_out:
 		hashset.build(
 			(line.rstrip('\n') for line in f_in), f_out, pickler=string_pickler())
 
@@ -40,6 +58,7 @@ elif len(args) >= 2 and args[0] == '--test':
 			fneedles = None
 		else:
 			fneedles = sys.stdin
+			sys.stdin = None
 			needles = (s.rstrip('\n') for s in fneedles)
 
 		item = None
