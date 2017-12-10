c32ec4005eda65ebd0620c7cbbadbfda53415387
David Foerster <david.foerster@informatik.hu-berlin.de>
2017-12-10 19:26:57 +0100

    Remove unused utility method

--- hashset/util/iter.py
+++ hashset/util/iter.py
@@ -25,18 +25,6 @@ def stareach( func, iterable ):
 		func(*item)
 
 
-def iskip( iterable, skip ):
-	"""Returns an iterator that skips the the first 'skip' items of the iterator created from 'iterable'."""
-
-	it = iter(iterable)
-	try:
-		for ignored in range(skip):
-			next(it)
-	except StopIteration:
-		pass
-	return it
-
-
 def ichain( iterable, *suffix ):
 	"""Returns an iterator that lists all items of 'iterable' followed by the remainder of the given arguments."""
 	return itertools.chain(iterable, suffix)
